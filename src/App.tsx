9a20100016cedfab2699582cb4d21a0393555a4a
Nathan Ferdinando <nferdica@outlook.com>
2024-08-26 23:12:33 -0300

    feat: Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
+import { Header } from "./components/header";
+
 export default function App() {
   return(
     <div>
-      <h1>Meu projeto!</h1>
+      <Header/>
+
+      <Aluno nome="Ana Caroline" idade={25} />
+      <Aluno nome="Nathan Ferdinando" idade={25} />
+
+    </div>
+  )
+}
+
+interface AlunoProps{
+  nome: string;
+  idade: number;
+}
+
+function Aluno({nome, idade}: AlunoProps) {
+  return(
+    <div>
+      <h1>Aluno: {nome}</h1>
+      <h3>Idade: {idade}</h3>
     </div>
   )
 }
